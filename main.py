fc2a2a5d2e01b9c57cacbeef4cab482aaa3424ac
Adam <ferdanbis@gmail.com>
2024-05-14 03:34:48 +0800

    Added Subtitle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import messagebox
 from ttkbootstrap import ttk, Style, PhotoImage
+import database
+
 
 # Set the default time for work and break intervals
 WORK_TIME = 5 * 60
@@ -80,34 +82,39 @@ class PomodoroTimer:
 
         self.settings_subtitle_label = ttk.Label(self.settings_frame, text="Timer", font=("TkDefaultFont", 18,"bold"))
         self.settings_subtitle_label.place(relx=0.5, rely=0.05,anchor=tk.CENTER)
+        self.settings_subtitle_label = ttk.Label(self.settings_frame, text="Save your own customize timer!", font=("TkDefaultFont", 10,"bold"))
+        self.settings_subtitle_label.place(relx=0.5, rely=0.095,anchor=tk.CENTER)
 
         timer_icon = self.icons["timer_icon"]
         self.settings_icon_label = ttk.Label(self.settings_frame,image=timer_icon)
         self.settings_icon_label.place(relx=0.46, rely=0.049, anchor = tk.CENTER)
 
         self.work_label = ttk.Label(self.settings_frame, text="Work Time (minutes):", font=("TKDefaultFont",10))
-        self.work_label.place(relx=0.5, rely=0.11,anchor=tk.CENTER)
+        self.work_label.place(relx=0.5, rely=0.14,anchor=tk.CENTER)
         self.work_entry = ttk.Entry(self.settings_frame)
         self.work_entry.insert(0, str(WORK_TIME//60))
-        self.work_entry.place(relx=0.5, rely=0.15,anchor=tk.CENTER)
+        self.work_entry.place(relx=0.5, rely=0.18,anchor=tk.CENTER)
 
         self.short_break_label = ttk.Label (self.settings_frame, text="Short Break Time (minutes):",font=("TKDefaultFont",10))
-        self.short_break_label.place(relx=0.5, rely=0.19,anchor=tk.CENTER)
+        self.short_break_label.place(relx=0.5, rely=0.22,anchor=tk.CENTER)
         self.short_break_entry = ttk.Entry(self.settings_frame)
         self.short_break_entry.insert(0, str(SHORT_BREAK_TIME // 60))
-        self.short_break_entry.place(relx=0.5, rely=0.23,anchor=tk.CENTER)
+        self.short_break_entry.place(relx=0.5, rely=0.26,anchor=tk.CENTER)
 
         self.long_break_label = ttk.Label(self.settings_frame, text="Long Break Time (minutes):",font=("TKDefaultFont",10))
-        self.long_break_label.place(relx=0.5, rely=0.27,anchor=tk.CENTER)
+        self.long_break_label.place(relx=0.5, rely=0.3,anchor=tk.CENTER)
         self.long_break_entry = ttk.Entry(self.settings_frame)
         self.long_break_entry.insert(0, str(LONG_BREAK_TIME // 60))
-        self.long_break_entry.place(relx=0.5, rely=0.31,anchor=tk.CENTER)
+        self.long_break_entry.place(relx=0.5, rely=0.34,anchor=tk.CENTER)
 
         self.save_button = ttk.Button(self.settings_frame, text="Save", command=self.save_settings)
-        self.save_button.place(relx=0.5, rely=0.37,anchor=tk.CENTER)
+        self.save_button.place(relx=0.5, rely=0.41,anchor=tk.CENTER)
 
         self.preadded_timers_label = ttk.Label(self.settings_frame, text="Pre-added Timers",font=("TkDefaulFont",18,"bold"))
         self.preadded_timers_label.place(relx=0.5, rely=0.47, anchor=tk.CENTER)
+        self.settings_subtitle_label = ttk.Label(self.settings_frame, text="Or try out one of our preadded-timers!", font=("TkDefaultFont", 10,"bold"))
+        self.settings_subtitle_label.place(relx=0.5, rely=0.51,anchor=tk.CENTER)
+
         preadded_icon = self.icons["preadded_icon"]
         self.settings_icon_label = ttk.Label(self.settings_frame,image=preadded_icon)
         self.settings_icon_label.place(relx=0.41, rely=0.47, anchor = tk.CENTER)
@@ -117,7 +124,7 @@ class PomodoroTimer:
         self.preadded_timers_listbox.insert(2, "    Light Work Session")
         self.preadded_timers_listbox.insert(3, "    Exercise Session")
         self.preadded_timers_listbox.insert(4, "    Meditation Session")
-        self.preadded_timers_listbox.place(relx=0.5, rely=0.57, anchor=tk.CENTER)
+        self.preadded_timers_listbox.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
 
         self.timer_descriptions = {
         "Deep Work Session": "1 hour of focused work followed by a 15-minute break.",
@@ -127,10 +134,10 @@ class PomodoroTimer:
         }
 
         self.timer_description_label = ttk.Label(self.settings_frame, text="", font=("TkDefaultFont", 12), wraplength=500)
-        self.timer_description_label.place(relx=0.5, rely=0.67, anchor=tk.CENTER)
+        self.timer_description_label.place(relx=0.5, rely=0.685, anchor=tk.CENTER)
         
         self.add_timer_button = ttk.Button(self.settings_frame, text="Try Out!", command=self.add_selected_timer)
-        self.add_timer_button.place(relx=0.5, rely=0.75, anchor=tk.CENTER)
+        self.add_timer_button.place(relx=0.5, rely=0.74, anchor=tk.CENTER)
         self.preadded_timers_listbox.bind("<<ListboxSelect>>", self.update_description_label)
 
     def update_description_label(self, event):
