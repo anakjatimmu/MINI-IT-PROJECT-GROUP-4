4ddbf2b59d40fa7b90e23dd94497f2f9b9ab5f29
Adam <ferdanbis@gmail.com>
2024-06-15 21:11:57 +0800

    Task

--- database.py
+++ database.py
@@ -6,6 +6,8 @@ def initialize_db():
     c = conn.cursor()
     c.execute("""CREATE TABLE IF NOT EXISTS profiles
                 (name text, work_time integer, short_break_time integer, long_break_time integer)""")
+    c.execute("""CREATE TABLE IF NOT EXISTS tasks
+                (profile_name text, task text)""")
     conn.commit()
     conn.close()
 def save_profile(profile_name, work_time, short_break_time, long_break_time):
@@ -31,9 +33,33 @@ def update_profile(new_name, work_time, short_break_time, long_break_time, old_n
                 WHERE name =?""", (new_name, work_time, short_break_time, long_break_time, old_name))
     conn.commit()
     conn.close()
+    
 def delete_profile(profile_name):
     conn = sqlite3.connect("pomodoro_timer.db")
     c = conn.cursor()
     c.execute("DELETE FROM profiles WHERE name=?", (profile_name,))
+    conn.commit()
+    conn.close()
+
+def add_task(profile_name, task):
+    conn = sqlite3.connect("pomodoro_timer.db")
+    c = conn.cursor()
+    c.execute("INSERT INTO tasks (profile_name, task) VALUES (?, ?)",
+              (profile_name, task))
+    conn.commit()
+    conn.close()
+
+def get_tasks(profile_name):
+    conn = sqlite3.connect("pomodoro_timer.db")
+    c = conn.cursor()
+    c.execute("SELECT task FROM tasks WHERE profile_name=?", (profile_name,))
+    tasks = c.fetchall()
+    conn.close()
+    return [task[0] for task in tasks]
+
+def remove_task(profile_name, task):
+    conn = sqlite3.connect("pomodoro_timer.db")
+    c = conn.cursor()
+    c.execute("DELETE FROM tasks WHERE profile_name=? AND task=?", (profile_name, task))
     conn.commit()
     conn.close()
